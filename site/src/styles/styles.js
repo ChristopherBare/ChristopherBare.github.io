77aff4a63c7cc1ae6bd5da6a6c729ffbdf951d6f
Christopher Bare <christopherbare@outlook.com>
2023-09-13 23:20:01 -0400

    added some colors

---
+++ site/src/styles/styles.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+const siteColors = {
+    bg: `#112340`,
+    lineHighlight: `#1d2d50`,
+    blue: `#5ccfe6`,
+    purple: `#c3a6ff`,
+    green: `#bae67e`,
+    yellow: `#ffd580`,
+    orange: `#ffae57`,
+    red: `#ef6b73`,
+    grey: `#a2aabc`,
+    comment: `#8695b799`,
+};
