19ce9083477cd8141c9ad5e853f694ac720fb449
Christopher Bare <christopherbare@outlook.com>
2023-10-16 19:07:51 -0400

    Working on site

--- site/src/navbar/navbar.js
+++ site/src/navbar/navbar.js
@@ -1,17 +1,20 @@
 // Navbar.js
 import logo from '../logo.svg';
 import '../styles/navbar.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
+function Header() {
     return (
-        <nav className="Navbar">
-            <div className="Navbar-logo">
-                <img src={logo} className="App-logo" alt="logo" />
-            </div>
-            <p className="Navbar-title">
-                Home
-            </p>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" className="Navbar">
+            <Container>
+                <Navbar.Brand className="Navbar-logo">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </Navbar.Brand>
+            </Container>
+
+        </Navbar>
         );
 }
 
