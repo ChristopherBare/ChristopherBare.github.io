e179f4505c21acec31170c046b2b0a3ff32e94cf
Christopher Bare <christopherbare@outlook.com>
2023-10-18 22:35:12 -0400

    added form and button to contact page

--- site/src/about/about.js
+++ site/src/about/about.js
@@ -4,9 +4,18 @@ import {siteColors} from '../styles/styles'
 function About() {
     return (
         <header className="App-header" id="about">
-            <div className="Header justify-content-end" style={{fontFamily: 'NotoMono-Regular'}}>
-                <h1>This is the about page.</h1>
+            <div className="Header" style={{fontFamily: 'NotoMono-Regular'}}>
+                <h1 className="justify-content-start">About Me</h1>
                 <p style={{color: siteColors.green}}>hello, friend.</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
+                <p>some stuff about me</p>
             </div>
         </header>
     );
--- site/src/contact/contact.js
+++ site/src/contact/contact.js
@@ -1,12 +1,25 @@
 // Header.js
 import {siteColors} from '../styles/styles'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Contact() {
     return (
         <header className="App-header" id="contact">
             <div className="Header justify-content-end" style={{fontFamily: 'NotoMono-Regular'}}>
-                <h1>This is the contact form.</h1>
+                <h1>Send me a message</h1>
                 <p style={{color: siteColors.green}}>hello, friend.</p>
+                <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Message</Form.Label>
+                        <Form.Control as="textarea" rows={6} />
+                    </Form.Group>
+                </Form>
+                <Button className="btn btn-lg btn-primary">Send</Button>
             </div>
         </header>
     );
