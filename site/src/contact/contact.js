8cc7e6eec063e62137486fc7230452da698e1b5f
Christopher Bare <christopherbare@outlook.com>
2023-10-20 17:50:11 -0400

    added contact css

--- site/src/contact/contact.js
+++ site/src/contact/contact.js
@@ -1,25 +1,33 @@
 // Header.js
 import {siteColors} from '../styles/styles'
+import '../styles/contact.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 function Contact() {
     return (
         <header className="App-header" id="contact">
+            <Container style={{ width: '40rem'}}>
             <div className="Header" style={{fontFamily: 'NotoMono-Regular'}}>
                 <h3 style={{color: siteColors.green}}>send me a message.</h3>
-                <Form style={{ width: '200%'}}>
-                    <Form.Group className="md" controlId="exampleForm.ControlInput1">
-                        <Form.Label>Email address</Form.Label>
+                <Form>
+                    <Form.Group className="md form" controlId="exampleForm.ControlInput1">
+                        <Form.Label style={{fontSize: "1.3rem"}}>email address</Form.Label>
                         <Form.Control type="email" placeholder="name@example.com" />
                     </Form.Group>
-                    <Form.Group className="mb-6" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>Message</Form.Label>
+                    <Form.Group className="md form" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label style={{fontSize: "1.3rem"}}>message</Form.Label>
                         <Form.Control as="textarea" rows={6} />
                     </Form.Group>
                 </Form>
-                <Button className="btn btn-lg btn-primary">Send</Button>
+                <br/>
+                <div className="d-grid gap-2">
+                    <Button size="lg" variant="outline-success">Send</Button>
+                </div>
+
             </div>
+            </Container>
         </header>
     );
 }
