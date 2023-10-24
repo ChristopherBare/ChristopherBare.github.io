e6321c48505f485165965a2336a01305c1683da7
Christopher Bare <christopherbare@outlook.com>
2023-10-24 16:08:10 -0400

    handler

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 resource "aws_lambda_function" "send_email_lambda" {
   function_name = "sendEmailLambda"
   role          = aws_iam_role.email_lambda_role.arn
-  handler       = "lambda.handler"
+  handler = "lambda.lambda_handler"
   runtime       = "python3.8"
 
   environment {
     variables = {
       TO_EMAIL   = "christopherbare@outlook.com",
-      FROM_EMAIL = "your_verified_ses_email@example.com",  # Replace with your verified SES email
+      FROM_EMAIL = "your_verified_ses_email@example.com", # Replace with your verified SES email
       REGION     = "us-east-1"
     }
   }
-  filename = "lambda_deployment_package.zip"
-  source_code_hash = filebase64sha256("lambda_deployment_package.zip")
+  filename         = "lambdas/email_lambda/lambda_deployment_package.zip"
+  source_code_hash = filebase64sha256("lambdas/email_lambda/lambda_deployment_package.zip")
 }
 
 resource "aws_iam_role" "email_lambda_role" {
@@ -79,14 +79,22 @@ resource "aws_iam_policy" "email_lambda_policy" {
   })
 }
 
+resource "aws_lambda_permission" "apigw_invoke_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.send_email_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
 resource "aws_iam_role_policy_attachment" "attach_email_lambda_policy" {
   policy_arn = aws_iam_policy.email_lambda_policy.arn
   role       = aws_iam_role.email_lambda_role.name
 }
 
+
 resource "aws_api_gateway_rest_api" "email_api" {
-  name        = "example-api"
-  description = "Example API for React app"
+  name        = "email-api"
+  description = "Email API"
 }
 
 resource "aws_api_gateway_resource" "root" {
@@ -108,41 +116,81 @@ resource "aws_api_gateway_integration" "lambda" {
   http_method = aws_api_gateway_method.post.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.send_email_lambda.invoke_arn
+  type                   = "AWS_PROXY"
+  uri                    = aws_lambda_function.send_email_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "response_200" {
+#resource "aws_api_gateway_method_response" "Success" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 200
+#
+#
+#
+#  response_models = {
+#    "application/json" = "Empty"
+#  }
+#}
+#
+#resource "aws_api_gateway_method_response" "default" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 200
+#
+#
+#
+#  response_models = {
+#    "application/json" = "Empty"
+#  }
+#}
+#
+#resource "aws_api_gateway_method_response" "default_400" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 400
+#
+#
+#
+#  response_models = {
+#    "application/json" = "Empty"
+#  }
+#}
+#
+#resource "aws_api_gateway_integration_response" "Success" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 200
+#
+#
+#}
+#
+#resource "aws_api_gateway_integration_response" "default" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 200
+#
+#
+#}
+#
+#resource "aws_api_gateway_integration_response" "default_400" {
+#  rest_api_id = aws_api_gateway_rest_api.email_api.id
+#  resource_id = aws_api_gateway_resource.root.id
+#  http_method = aws_api_gateway_method.post.http_method
+#  status_code = 400
+#
+#
+#}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+
+  ]
   rest_api_id = aws_api_gateway_rest_api.email_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.post.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty",
-  }
-}
-
-resource "aws_api_gateway_integration_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.email_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.post.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on  = [aws_api_gateway_integration.lambda]
-  rest_api_id = aws_api_gateway_rest_api.email_api.id
-  stage_name  = "prod"
-}
-resource "aws_lambda_permission" "api_gateway_invoke" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.send_email_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = aws_api_gateway_deployment.api_deployment.execution_arn
+  stage_name = "v1"
 }
