160bb9d819e93b10cbdbe60cbef51a6a30decc21
Christopher Bare <christopherbare@outlook.com>
2023-11-12 00:09:59 -0500

    updated the code block

--- site/src/helper-components/code-block.js
+++ site/src/helper-components/code-block.js
@@ -1,11 +1,12 @@
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs'; // Import the Dracula style
 import PropTypes from 'prop-types';
+import '../styles/code-block.css'
 
 const CodeBlockComponent = ({ code, lang }) => {
     return (
-        <div style={{ width: '100%', overflowX: 'auto' }}>
-            <SyntaxHighlighter language={lang} style={dracula}>
+        <div style={{ maxWidth: '1000px',maxHeight: '600px', overflow: 'auto', borderRadius: "10px"}}>
+            <SyntaxHighlighter className="json-code-block" language={lang} style={dracula}>
                 {code}
             </SyntaxHighlighter>
         </div>
--- site/src/resume/resume.js
+++ site/src/resume/resume.js
@@ -2,16 +2,20 @@
 // import {siteColors} from '../styles/styles'
 import useAPI from '../custom-hooks/useAPI'
 import CodeBlockComponent from '../helper-components/code-block'
+import {siteColors} from "../styles/styles";
+
 
 
 function Resume() {
     const apiUrl = 'https://sp14jyfina.execute-api.us-east-1.amazonaws.com/prod/resume'
     const { data, isLoading, error } = useAPI(apiUrl)
     const jsonString = JSON.stringify(data, null, 2)
+    const formattedCode = JSON.stringify(JSON.parse(jsonString), null, 2);
     return (
         <header className="App-header" id="resume">
-            <div className="Header justify-content-end" style={{fontFamily: 'NotoMono-Regular'}}>
-                <h1>This is the resume page.</h1>
+            <div className="Header" style={{fontFamily: 'NotoMono-Regular'}}>
+                <h1>Resume</h1>
+                <p style={{color: siteColors.green}}>This is my <a style={{textDecoration: "none"}} href="https://github.com/ChristopherBare/resum-api">resum-api</a></p>
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : error ? (
@@ -19,7 +23,7 @@ function Resume() {
                 ) : (
                     //this will be a styled code block component in the future
                     //TODO implement code block component here after getting resume api to work
-                    <CodeBlockComponent code={jsonString} lang="javascript" showLineNumbers={true} />
+                    <CodeBlockComponent code={formattedCode} lang="json" showLineNumbers={true} />
                 )}
             </div>
         </header>
