dd141306a34740b186e0e3778faba33790a971ba
Christopher Bare <christopherbare@outlook.com>
2025-08-09 00:18:01 -0400

    Temporarily hard code api invoke url

--- site/src/contact/contact.js
+++ site/src/contact/contact.js
@@ -24,7 +24,7 @@ function Contact() {
 
         try {
             // Get the API Gateway URL from your Terraform output
-            const apiUrl = `${}/v1`;
+            const apiUrl = `https://4voyg0q44c.execute-api.us-east-1.amazonaws.com/v1`;
             
             const response = await fetch(apiUrl, {
                 method: 'POST',
